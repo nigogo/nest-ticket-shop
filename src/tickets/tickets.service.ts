fadef1ddb77dc7549d908c6ca2beb370a3918d68
Nico Harather <nicoharather@gmail.com>
2024-10-02 21:53:31 +0200

    Wrap ticket creation in transaction

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,20 +1,12 @@
 import { ConflictException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Ticket } from './ticket.entity';
-import { Repository } from 'typeorm';
-import { EventsService } from '../events/events.service';
+import { DataSource } from 'typeorm';
 import { TicketDto } from './dto/ticket.dto';
 import { Event } from '../events/event.entity';
 
 @Injectable()
 export class TicketsService {
-	constructor(
-		private eventService: EventsService,
-		@InjectRepository(Ticket)
-		private ticketRepository: Repository<Ticket>,
-		@InjectRepository(Event)
-		private eventRepository: Repository<Event>
-	) {}
+	constructor(private dataSource: DataSource) {}
 
 	async create({
 		eventId,
@@ -23,30 +15,46 @@ export class TicketsService {
 		eventId: number;
 		userId: number;
 	}): Promise<TicketDto> {
-		const event = await this.eventService.findOne(eventId);
-		if (event.available_tickets <= 0) {
-			throw new ConflictException('No available tickets');
+		const queryRunner = this.dataSource.createQueryRunner();
+		await queryRunner.connect();
+		await queryRunner.startTransaction();
+
+		try {
+			const event = await queryRunner.manager.findOneOrFail(Event, {
+				where: { id: eventId },
+				lock: { mode: 'pessimistic_write' },
+			});
+
+			if (event.available_tickets <= 0) {
+				throw new ConflictException('No available tickets');
+			}
+
+			const ticket = queryRunner.manager.create(Ticket, {
+				user: { id: userId },
+				event: { id: eventId },
+				price_paid: event.ticket_price,
+				purchase_time: new Date(),
+			});
+			const storedTicket = await queryRunner.manager.save(Ticket, ticket);
+
+			await queryRunner.manager.update(Event, eventId, {
+				available_tickets: event.available_tickets - 1,
+			});
+
+			await queryRunner.commitTransaction();
+
+			return {
+				id: storedTicket.id,
+				user_id: Number(storedTicket.user.id),
+				event_id: Number(storedTicket.event.id),
+				price_paid: storedTicket.price_paid,
+				purchase_time: storedTicket.purchase_time,
+			};
+		} catch (error) {
+			await queryRunner.rollbackTransaction();
+			throw error;
+		} finally {
+			await queryRunner.release();
 		}
-
-		const ticket = this.ticketRepository.create({
-			user: { id: userId },
-			event: { id: eventId },
-			price_paid: event.ticket_price,
-			purchase_time: new Date(),
-		});
-		const storedTicket = await this.ticketRepository.save(ticket);
-
-		// decrement available tickets
-		await this.eventRepository.update(eventId, {
-			available_tickets: event.available_tickets - 1,
-		});
-
-		return {
-			id: storedTicket.id,
-			user_id: Number(storedTicket.user.id),
-			event_id: Number(storedTicket.event.id),
-			price_paid: storedTicket.price_paid,
-			purchase_time: storedTicket.purchase_time,
-		};
 	}
 }
